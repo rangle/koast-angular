978c61da1ac485cf820cc6b5e50850414949e1f5
Brian Olynyk <brian@rangle.io>
2015-01-08 11:20:08 -0500

    Story: 85647574
    Added test cases for koastTokenKeeper service.

--- src/core/koast-http/koast-token-keeper-service.test.js
+++ src/core/koast-http/koast-token-keeper-service.test.js
@@ -5,17 +5,25 @@
 'use strict';
 
 describe('_koastTokenKeeper',function(){
+  var _koastTokenKeeper;
+  var $window;
+  var tokenKey;
+  var tokenValue;
+
   beforeEach(module('koast-http'));
 
   beforeEach(module(function ($provide) {
     $provide.service('$q', function() {return Q;});
   }));
 
-  var _koastTokenKeeper;
   beforeEach(function(){
     inject(function($injector){
       _koastTokenKeeper = $injector.get('_koastTokenKeeper');
+      $window = $injector.get('$window');
     });
+    tokenKey = 'testKey';
+    tokenValue = 'testValue';
+    _koastTokenKeeper.setTokenKey(tokenKey);
   });
 
   it('should exist', function() {
@@ -26,6 +34,23 @@ describe('_koastTokenKeeper',function(){
     expect(_koastTokenKeeper.clear).to.be.a('function');
   });
 
-  //TODO: write tests for _koastTokenKeeper methods
+  it('should save a token', function() {
+    _koastTokenKeeper.saveToken({token: tokenValue});
+    expect($window.localStorage[tokenKey]).equals(tokenValue);
+  });
 
+  it('should load a token after it is saved', function() {
+    _koastTokenKeeper.saveToken(tokenValue);  //Using alternate API on purpose.
+    expect($window.localStorage[tokenKey]).equals(tokenValue);
+    var loadedValue = _koastTokenKeeper.loadToken(tokenKey);
+    expect(loadedValue).equals(tokenValue);
+  });
+
+  it('should clear a token', function () {
+    _koastTokenKeeper.saveToken(tokenValue);
+    _koastTokenKeeper.clear(tokenKey);
+    var loadedValue = _koastTokenKeeper.loadToken(tokenKey);
+    expect($window.localStorage[tokenKey]).to.be.undefined();
+    expect(loadedValue).to.be.null();
+  });
 });
